30e3b98f9cb109b3347a62ef7117cf7a2d5eb744
Tawhid-sw <tawhidsarkar684@gmail.com>
2024-10-17 08:04:08 +0600

    Added React Router Dom and Make pages for diffrent categories also add title with icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <main>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-        Hello world!{" "}
-      </h1>
+      <Outlet />
     </main>
   );
 }
